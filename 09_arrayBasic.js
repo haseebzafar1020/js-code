a85ea3dadedc4ba891b6f76cc6b7b2edded7b5c8
Haseeb Zafar 1020 <haseebmalikgoto@gmail.com>
2025-08-09 00:42:37 -0700

    Array basics

---
+++ 09_arrayBasic.js
@@ -0,0 +1,51 @@
+const arr = [1,2,3,4,5]
+const arr1 = ["ahsan" ,"haseeb"]
+
+const arr2 = new Array(1,2,3,4,5)
+// console.log(arr2[3]);
+
+//-->Array Method
+
+
+// arr.push(6)
+// console.log(arr);
+
+// arr.push(7)
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+// arr.unshift(9)
+// console.log(arr); // start ma 9 ajay gay sab eliment ko agy shift kaar kay...
+
+// arr.shift()
+// console.log(arr); //start wala element remove hu jay ga...
+
+
+
+// console.log(arr.includes(9)); // bolean data type 
+// console.log(arr.indexOf(9)); // bolean
+
+// const newArr =  arr.join() // join method change ker data ha array ki typeof "string" ma..
+// console.log(arr);
+// console.log(newArr);
+
+
+const arn1 = arr.slice(1,3)  //ya Array ma se baki data ko remove kar data ha 
+console.log(arr);
+console.log("A ", arn1)  
+
+
+const myn2 = arr.splice(1,3) // ya array ma se 
+console.log(arr)
+console.log("B" , myn2);
+
+
+
+
+
+
+
+
+
